3562d08e4912371e64477b798ea8d09767861ab3
baraa haydar <baraa97.bh@gmail.com>
2021-04-02 14:46:27 +0300

    step12 13

--- index.js
+++ index.js
@@ -43,77 +43,88 @@ app.get('/search', (req, res) => {
     }
 });
 
-app.get('/movies/', (req, res) => {
-    res.status(200).send(movies)
-});
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb-connection-string', (err, client) => { });
 
-app.get('/movies/:by', (req, res) => {
-    if (req.params.by == '' || req.params.by == undefined) {
+const uri = 'mongodb+srv://moustafa_kassem12:Moustafa%2F123@cluster0.jcdl8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+MongoClient.connect(uri, { useUnifiedTopology: true })
+
+.then(client => {
+    console.log('Connected to Database')
+    const db = client.db('datas')
+
+    app.get('/movies/', (req, res) => {
         res.status(200).send(movies)
-    }
-    else if (req.params.by == 'by-date') {
-        movies.sort((a, b) => { return a.year - b.year; })
-        res.status(200).send(movies);
-    }
-    else if (req.params.by == 'by-rating') {
-        movies.sort((a, b) => { return b.rating - a.rating; })
-        res.status(200).send(movies);
-    }
-    else if (req.params.by == 'by-title') {
-        movies.sort((a, b) => { return a.title.localeCompare(b.title); })
-        res.status(200).send(movies);
-    }
-});
+    });
 
-app.get('/movies/read/id/:id', (req, res) => {
-    if (req.params.id <= 0 || req.params.id > movies.length) {
-        res.status(404).send('The movie ' + req.params.id + ' does not exist');
-    }
-    else {
-        res.status(200).send(movies[req.params.id - 1])
-    }
-});
+    app.get('/movies/:by', (req, res) => {
+        if (req.params.by == '' || req.params.by == undefined) {
+            res.status(200).send(movies)
+        }
+        else if (req.params.by == 'by-date') {
+            movies.sort((a, b) => { return a.year - b.year; })
+            res.status(200).send(movies);
+        }
+        else if (req.params.by == 'by-rating') {
+            movies.sort((a, b) => { return b.rating - a.rating; })
+            res.status(200).send(movies);
+        }
+        else if (req.params.by == 'by-title') {
+            movies.sort((a, b) => { return a.title.localeCompare(b.title); })
+            res.status(200).send(movies);
+        }
+    });
 
-app.post('/movies', (req, res) => {
-    var t = req.query.title;
-    var y = req.query.year;
-    var r = req.query.rating;
-    if (t == "" || y == "" || y.toString().length != 4 || isNaN(y)) { res.status(403).send('You cannot create a movie without providing a title and a year') }
-    else {
-        if (r == "" || typeof r == undefined) { r = 4; }
-    }
-    movies.push({ title: t, year: parseInt(y), rating: parseInt(r) });
-    res.status(200).send(movies)
-});
+        app.get('/movies/read/id/:id', (req, res) => {
+            if (req.params.id <= 0 || req.params.id > movies.length) {
+                res.status(404).send('The movie ' + req.params.id + ' does not exist');
+            }
+            else {
+                res.status(200).send(movies[req.params.id - 1])
+            }
+        });
 
-app.delete('/movies/:id', (req, res) => {
-    if (req.params.id < 1 || req.params.id > movies.length) {
-        res.status(404).send('The movie ' + req.params.id + ' does not exist')
-    }
-    else {
-        movies.splice(req.params.id - 1, 1)
-        res.send(movies)
-    }
-});
+        app.post('/movies', (req, res) => {
+            var t = req.query.title;
+            var y = req.query.year;
+            var r = req.query.rating;
+            if (t == "" || y == "" || y.toString().length != 4 || isNaN(y)) { res.status(403).send('You cannot create a movie without providing a title and a year') }
+            else {
+                if (r == "" || typeof r == undefined) { r = 4; }
+            }
+            movies.push({ title: t, year: parseInt(y), rating: parseInt(r) });
+            res.status(200).send(movies)
+        });
 
-app.put('/movies/:id', (req, res) => {
-    var id = req.params.id
-    if (id <= 0 || id > movies.length) {
-        res.status(404).send('The movie ' + id + ' doesn\'t exist');
-    }
-    else { var movie = movies[id - 1]; }
-    var arrV = Object.values(movie);
-    var t = req.query.title;
-    var y = req.query.year;
-    var r = req.query.rating;
-    if ((t == "" || t == undefined) && (r == "" || r == undefined) && (y == "" || y == undefined)) {
-        res.status(404).send('Nothing to update');
-    } else {
-        (t == undefined || t == "") ? movie['title'] = arrV[0] : movie['title'] = t;
-        (y == undefined || y == "") ? movie['year'] = arrV[1] : movie['year'] = parseInt(y);
-        (r == undefined || r == "") ? movie['rating'] = arrV[2] : movie['rating'] = parseInt(r);
-        res.send(movie);
-    }
-});
+        app.delete('/movies/:id', (req, res) => {
+            if (req.params.id < 1 || req.params.id > movies.length) {
+                res.status(404).send('The movie ' + req.params.id + ' does not exist')
+            }
+            else {
+                movies.splice(req.params.id - 1, 1)
+                res.send(movies)
+            }
+        });
 
-app.listen(port, () => console.log(`Success! Your application is running on \"http://localhost:${port}\"`));
+        app.put('/movies/:id', (req, res) => {
+            var id = req.params.id
+            if (id <= 0 || id > movies.length) {
+                res.status(404).send('The movie ' + id + ' doesn\'t exist');
+            }
+            else { var movie = movies[id - 1]; }
+            var arrV = Object.values(movie);
+            var t = req.query.title;
+            var y = req.query.year;
+            var r = req.query.rating;
+            if ((t == "" || t == undefined) && (r == "" || r == undefined) && (y == "" || y == undefined)) {
+                res.status(404).send('Nothing to update');
+            } else {
+                (t == undefined || t == "") ? movie['title'] = arrV[0] : movie['title'] = t;
+                (y == undefined || y == "") ? movie['year'] = arrV[1] : movie['year'] = parseInt(y);
+                (r == undefined || r == "") ? movie['rating'] = arrV[2] : movie['rating'] = parseInt(r);
+                res.send(movie);
+            }
+        });
+        app.listen(port, () => console.log(`Success! Your application is running on \"http://localhost:${port}\"`));
+    })
+    .catch(error => console.error(error)) 
